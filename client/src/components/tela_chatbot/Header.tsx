5737a839b878d411f3e9ab9f7a9618953b6f0280
Fabriely Santos <fabrielysantos15@gmail.com>
2025-01-31 15:19:53 -0300

    feat(history-summary): modal signout

--- client/src/components/tela_chatbot/Header.tsx
+++ client/src/components/tela_chatbot/Header.tsx
@@ -4,14 +4,15 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 import { Logo } from '../../assets';
 import LoginModal from '../modalLogin';
+import LogoutModal from '../modalLogout';  // Importa o novo modal de logout
 
 const Header: React.FC = () => {
   const [showLoginModal, setShowLoginModal] = React.useState(false);
+  const [showLogoutModal, setShowLogoutModal] = React.useState(false); // Estado para controlar o modal de logout
   const { data: session } = useSession();
   const router = useRouter();
-  const pathname = usePathname(); // Obtém a rota atual
+  const pathname = usePathname();
 
-  // Verifica se está na página "Meus Documentos"
   const isOnDocumentsPage = pathname === "/mydocuments";
 
   return (
@@ -30,19 +31,20 @@ const Header: React.FC = () => {
       <div className="flex items-center space-x-4">
         {session ? (
           <>
-            <button onClick={() => router.push(isOnDocumentsPage ? "/chatbot" : "/mydocuments")}
-              >
+            <button onClick={() => router.push(isOnDocumentsPage ? "/chatbot" : "/mydocuments")}>
               {isOnDocumentsPage ? "ChatBot" : "Meus Documentos"}
             </button>
             <a href="#my-profile" className="px-4 py-2 rounded-[24px] text-white font-medium bg-gradient-to-r from-[#004BD4] via-[#5331CF] via-[#7726CD] to-[#A219CA] hover:opacity-90">
               Meu Perfil
             </a>
-            <a href="/api/auth/signout" className="text-gray-700 font-medium hover:text-[#a219ca]">Sair</a>
+            <button onClick={() => setShowLogoutModal(true)} className="text-gray-700 font-medium hover:text-[#a219ca]">
+              Sair
+            </button>
+            {showLogoutModal && <LogoutModal onClose={() => setShowLogoutModal(false)} />}  {/* Exibe o modal de logout */}
           </>
         ) : (
           <>
-            <button onClick={() => setShowLoginModal(true)}
-              className="px-4 py-2 rounded-[24px] text-white font-medium bg-gradient-to-r from-[#004BD4] via-[#5331CF] via-[#7726CD] to-[#A219CA] hover:opacity-90">
+            <button onClick={() => setShowLoginModal(true)} className="px-4 py-2 rounded-[24px] text-white font-medium bg-gradient-to-r from-[#004BD4] via-[#5331CF] via-[#7726CD] to-[#A219CA] hover:opacity-90">
               Login
             </button>
             {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} />}
