755d599956c84b7557b802ec6874678b7c148f43
Fabriely Santos <fabrielysantos15@gmail.com>
2025-02-04 12:53:06 -0300

    feat(integration-analyze): hash password

--- server/routes/users.py
+++ server/routes/users.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from bcrypt import checkpw
 from sqlalchemy.orm import Session
 import crud, schemas, dependencies
 
@@ -12,6 +13,6 @@ async def create_user(request: schemas.UserCreate, db: Session = Depends(depende
 @router.post("/sessions")
 async def login(request: schemas.UserCreate, db: Session = Depends(dependencies.get_db)):
     user = crud.get_user_by_email(db, request.email)
-    if user and user.password == request.password:
-        return {"data": {"user": user}}
+    if user and checkpw(request.password.encode('utf-8'), user.password.encode('utf-8')):
+            return {"data": {"user": user}}
     raise HTTPException(status_code=401, detail="Invalid credentials")
