b9bfb4fc07d9c5c93d6354fad228d39e15994c86
Kleberson Araujo <112328070+KleberAraujoo@users.noreply.github.com>
2025-02-25 19:57:43 -0300

    Update route.ts

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,20 @@
 import NextAuth from 'next-auth';
 
-import { nextAuthOptions } from './options';
+import GoogleProvider from 'next-auth/providers/google';
+//import { nextAuthOptions } from './options';
 
-const handler = NextAuth(nextAuthOptions);
+//const handler = NextAuth(nextAuthOptions);
+const handler = NextAuth ({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        })
+    ],
+    pages: {
+        signIn: '/login',
+        error: '/login',
+    },
+})
 
 export { handler as GET, handler as POST };
