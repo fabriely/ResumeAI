fa359ad14134f848bb72aa7a31ca4a7d4f804214
João Pedro A. de Moraes <160938086+SunIord@users.noreply.github.com>
2025-03-01 09:27:09 -0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -25,3 +25,12 @@ class Summary(Base):
 
     user = relationship("User", back_populates="summaries")
 
+class Analysis(Base):
+    __tablename__= "analysis"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    file_name = Column(String, nullable=False)
+    file_path = Column(String, nullable=False)
+    user_email = Column(String, ForeignKey("users.email"))
+
+    user = relationship("User", backref="analyses")
