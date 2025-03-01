cbe89d32534c8aeee80eba8e602fcf9f8b1f80de
João Pedro A. de Moraes <160938086+SunIord@users.noreply.github.com>
2025-03-01 09:26:10 -0300

    Update crud.py

--- server/crud.py
+++ server/crud.py
@@ -1,67 +1,36 @@
-# app/crud.py
-from bcrypt import hashpw, gensalt, checkpw
-from sqlalchemy.orm import Session
-from models import User, Summary
-import schemas
-import json
+# app/models.py
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
 
-def create_user(db: Session, name: str, last_name: str, email: str, password: str):
-    # Gerando o salt e o hash da senha
-    hashed_password = hashpw(password.encode('utf-8'), gensalt())
+Base = declarative_base()
   
-    # Criando o usuário com todas as informações obrigatórias
-    user = User(
-      name = name,
-      last_name = last_name,
-      email = email,
-      password = hashed_password.decode('utf-8')
-    )
-  
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = Column(String) 
+    last_name = Column(String)  
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    summaries = relationship("Summary", back_populates="user")
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+class Summary(Base):
+    __tablename__ = "summaries"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String, index=True)
+    user_email = Column(String, ForeignKey("users.email"))
 
-def add_summary(db: Session, email: str, summary_data: schemas.SummaryRequest):
-    user = db.query(User).filter(User.email == email).first()
-    if user:
-        # Serializando o dicionário para string JSON
-        summary_content = json.dumps(summary_data.content)  
-        new_summary = Summary(content=summary_content)
-        user.summaries.append(new_summary)
-        db.commit()
-        db.refresh(user)
-        return user
-    return None
+    user = relationship("User", back_populates="summaries")
 
-def get_summaries(db: Session, email: str):
-    user = db.query(User).filter(User.email == email).first()
-    return user.summaries if user else None
+class Analysis(Base):
+    __tablename__= "analysis"
 
-def delete_summary(db: Session, email: str, summary_id: int):
-    user = db.query(User).filter(User.email == email).first()
-    if user:
-        summary_to_delete = db.query(Summary).filter(Summary.id == summary_id, Summary.user_email == email).first() 
-        if summary_to_delete:
-            db.delete(summary_to_delete)
-            db.commit()
-            return True
-    return False
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    file_name = Column(String, nullable=False)
+    file_path = Column(String, nullable=False)
+    user_email = Column(String, ForeignKey("users.email"))
 
-def check_password(db: Session, email: str, password: str):
-    user = db.query(User).filter(User.email == email).first()
-    if user:
-        return checkpw(password.encode('utf-8'), user.password.encode('utf-8'))
-    return False
-
-def update_password(db: Session, email: str, new_password: str):
-    user = db.query(User).filter(User.email == email).first()
-    if user:
-        hashed_password = hashpw(new_password.encode('utf-8'), gensalt())
-        user.password = hashed_password.decode('utf-8')
-        db.commit()
-        return True
-    return False
+    user = relationship("User", backref="analyses")
