7504dfc0f961e4495af46c83b97ea60deb0196c0
fabriely <flps@cin.ufpe.br>
2025-03-12 14:14:03 -0300

    feat: add pytest integration for testing before server startup; update dependencies

--- server/main.py
+++ server/main.py
@@ -1,11 +1,14 @@
 # app/main.py
 from fastapi import FastAPI, HTTPException
+import uvicorn
 from pydantic import ValidationError
 from schemas.user_schema import LoginCredentials
 from sqlalchemy import create_engine
 from fastapi.middleware.cors import CORSMiddleware
 from dependencies import Base
 from routes import users, summaries, analyze
+import pytest
+
 
 DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
@@ -41,3 +44,15 @@ async def login(credentials: LoginCredentials):
         raise HTTPException(status_code=400, detail=e.errors())
 
     return {"success": True, "message": "Login realizado com sucesso!"}
+
+if __name__ == "__main__":
+    print("🔍 Executando testes antes de iniciar o servidor...")
+    result = pytest.main(["-q", "--disable-warnings"])
+
+    if result == 0:
+        print("✅ Todos os testes passaram! Iniciando o servidor...")
+        uvicorn.run(app, host="127.0.0.1", port=8000)
+
+    else:
+        print("❌ Testes falharam! Corrija os erros antes de iniciar o servidor.")
+
--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -1,13 +1,15 @@
+from models.user import User
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, User
+from dependencies import Base
 from services.user_service import create_user, check_user_password
 from utils.security import hash_password
 
 DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 @pytest.fixture
 def db():
@@ -18,9 +20,9 @@ def db():
     Base.metadata.drop_all(bind=engine)
 
 def test_create_user(db):
-    user = create_user(db, "John", "Doe", "john@example.com", "password123!")
+    user = create_user(db, "John", "Doe", "john@example.com", "Password@123")
     assert user.email == "john@example.com"
-    assert user.password != "password123!"
+    assert user.password != "Password@123"
 
 def test_check_user_password(db):
     hashed_pw = hash_password("securepassword!")
